b9adbd0b20b6034d3a2a7010f202779cc253fdab
badb0ttle <2546744220@qq.com>
2024-11-05 15:14:13 +0800

    a1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,24 +1,29 @@
 import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
 import { getDirname, path } from "vuepress/utils";
+import theme from "./theme.js";
+
 const __dirname = getDirname(import.meta.url);
+
 export default defineUserConfig({
   base: "/docs/",
   lang: "zh-CN",
   title: "Badbottle's blog",
   description: "Blnk is infinite.",
-  alias: {
-    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
-      __dirname,
-      "./components/BlogHero.vue",
-    ),
-  },
+
   theme,
 
   // 配置 favicon.ico 路径
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
+
+  // 配置别名
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue"
+    ),
+  },
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
