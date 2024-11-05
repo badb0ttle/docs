74986cf9a43f1264e2647eed0887c2bd00c3e6e5
badb0ttle <2546744220@qq.com>
2024-11-05 12:21:00 +0800

    修改了一些bug

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,19 @@
 import { defineUserConfig } from "vuepress";
-
 import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/docs/",
 
   lang: "zh-CN",
-  title: "Badbottle's blog",
+  title: "Badbottle's bl",
   description: "Blnk is infinite.",
 
   theme,
+
+  // 配置 favicon.ico 路径
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
