9a90a9048382ffc0d80f51e6884b6c544bd1d02b
badb0ttle <2546744220@qq.com>
2024-11-05 14:33:49 +0800

    f2

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,7 +11,7 @@ export default defineUserConfig({
 
   // 配置 favicon.ico 路径
   head: [
-    ['link', { rel: 'icon', href: 'D:/my-blog/blog/src/.vuepress/public/logo.ico' }]
+    ['link', { rel: 'icon', href: 'src/.vuepress/public/logo.ico' }]
   ],
 
   // 和 PWA 一起启用
