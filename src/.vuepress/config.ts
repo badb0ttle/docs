e001f450abcc9a4435e574624906d4069c020907
badb0ttle <2546744220@qq.com>
2024-11-05 15:03:26 +0800

    yiyan

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,12 +1,18 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-
+import { getDirname, path } from "vuepress/utils";
+const __dirname = getDirname(import.meta.url);
 export default defineUserConfig({
   base: "/docs/",
   lang: "zh-CN",
   title: "Badbottle's blog",
   description: "Blnk is infinite.",
-
+  alias: {
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",
+    ),
+  },
   theme,
 
   // 配置 favicon.ico 路径
