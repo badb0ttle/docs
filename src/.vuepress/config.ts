b87a88e06ec28802c26d27d4ae43c4cc068c8ea6
badb0ttle <2546744220@qq.com>
2024-11-05 10:43:25 +0800

    标题和描述

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,8 +6,8 @@ export default defineUserConfig({
   base: "/docs/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "✌︎(ツ)ɔ",
+  description: "Blnk is infinite.",
 
   theme,
 
