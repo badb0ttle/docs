1c68339e365335d92917e96dbd79c894786a3230
badb0ttle <2546744220@qq.com>
2024-11-07 11:28:11 +0800

    博客准备工作

--- src/.vuepress/components/BlogHero.vue
+++ src/.vuepress/components/BlogHero.vue
@@ -3,13 +3,13 @@
 console.log("BlogHero.vue");
 import BlogHero from "vuepress-theme-hope/blog/components/BlogHero.js";
 import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
+
 </script>
 
 <template>
   <BlogHero>
     <template #info="info">
       <HitokotoBlogHero v-bind="info" />
-      <div>这是测试内容，用于检查 Hitokoto 组件是否加载</div>
     </template>
   </BlogHero>
 </template>
